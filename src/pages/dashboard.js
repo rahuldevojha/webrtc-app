ee25b671b01390d1903284f3fca4e6e0351b6154
vinay156 <vgoyal@ch.iitr.ac.in>
2020-06-19 21:13:04 +0530

    dashboard frame

---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react"
+import DashBoard from "../components/doctorComponents/dashboard"
+
+const Dashboard = () => (
+  <div>
+    <DashBoard />
+  </div>
+)
+
+export default Dashboard
